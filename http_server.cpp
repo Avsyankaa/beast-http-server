5be97a5a21b93286421839b906a5a10983ad9987
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-11 11:36:26 +0300

    Update http_server.cpp

--- http_server.cpp
+++ http_server.cpp
@@ -1,176 +1,200 @@
-﻿#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/signal_set.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/config.hpp>
-
 #include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <thread>
 
-using tcp = boost::asio::ip::tcp;     // from <boost/asio/ip/tcp.hpp>
-namespace http = boost::beast::http;  // from <boost/beast/http.hpp>
-
-struct FrindlyUserServer
-{
-public:
-    FrindlyUserServer(boost::asio::ip::address address, unsigned short port)
-        : ioc {1}, acceptor {ioc, {address, port}}, signals(ioc) {
-        signals.async_wait(
-            [this](boost::system::error_code const&, int) { this->ioc.stop(); });
-    }
-
-    void AddStopSignal(int signal) { signals.add(signal); }
-
-    void Start()
-    {
-        for (;;) {
-            tcp::socket socket {ioc};
-
-            acceptor.accept(socket);
-
-            std::thread {
-                std::bind(&FrindlyUserServer::DoSession, this, std::move(socket))}
-            .detach();
-        }
-    }
-
-protected:
-    virtual void DoSession(tcp::socket& socket) = 0;
-
-private:
-    boost::asio::io_context ioc;
-    tcp::acceptor acceptor;
-    boost::asio::signal_set signals;
-};
-
-struct SimpleServer : public FrindlyUserServer
-{
-    SimpleServer(boost::asio::ip::address address, unsigned short port)
-        : FrindlyUserServer(address, port)
-    { }
-
-protected:
-    void DoSession(tcp::socket& socket) override
-    {
-        bool close = false;
-        boost::system::error_code ec;
-
-        // This buffer is required to persist across reads
-        boost::beast::flat_buffer buffer;
-
-        // This lambda is used to send messages
-        send_lambda<tcp::socket> send {socket, close, ec};
-
-        for (;;) {
-            // Read a request
-            http::request<http::string_body> req;
-            http::read(socket, buffer, req, ec);
-            if (ec == http::error::end_of_stream)
-                break;
-            if (ec)
-                return fail(ec, "read");
-
-            // Send the response
-            send(HandleRequest(std::move(req)));
-
-            if (ec)
-                return fail(ec, "write");
-            if (close) {
-                // This means we should close the connection, usually because
-                // the response indicated the "Connection: close" semantic.
-                break;
-            }
-        }
-
-        // Send a TCP shutdown
-        socket.shutdown(tcp::socket::shutdown_send, ec);
-    }
-
-    virtual http::response<http::string_body> HandleRequest(
-        http::request<http::string_body>&& req) = 0;
-
-private:
-    // This is the C++11 equivalent of a generic lambda.
-    // The function object is used to send an HTTP message.
-    template <class Stream>
-    struct send_lambda
-    {
-        Stream& stream_;
-        bool& close_;
-        boost::system::error_code& ec_;
-
-        explicit send_lambda(Stream& stream,
-                             bool& close,
-                             boost::system::error_code& ec)
-            : stream_(stream), close_(close), ec_(ec)
-        { }
-
-        template <bool isRequest, class Body, class Fields>
-        void operator()(http::message<isRequest, Body, Fields>&& msg) const
-        {
-            // Determine if we should close the connection after
-            close_ = msg.need_eof();
-
-            // We need the serializer here because the serializer requires
-            // a non-const file_body, and the message oriented version of
-            // http::write only works with const messages.
-            http::serializer<isRequest, Body, Fields> sr {msg};
-            http::write(stream_, sr, ec_);
-        }
-    };
-
-    void fail(boost::system::error_code ec, char const* what)
-    {
-        std::cerr << what << ": " << ec.message() << "\n";
-    }
-};
-
-struct HelloWorld : public SimpleServer
-{
-    HelloWorld(boost::asio::ip::address address, unsigned short port)
-        : SimpleServer(address, port)
-    { }
-
-protected:
-    http::response<http::string_body> HandleRequest(
-        http::request<http::string_body>&& req) override
-    {
-        http::response<http::string_body> res {http::status::ok, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, "application/json");
-        res.keep_alive(req.keep_alive());
-        const char* data = R"({
-          "response": {
-            "text": "Здравствуйте! Это мы, хороводоведы.",
-            "tts": "Здравствуйте! Это мы, хоров+одо в+еды.",
-            "buttons": [
-                {
-                    "title": "Надпись на кнопке",
-                    "payload": {},
-                    "url": "https://example.com/",
-                    "hide": true
-                }
-            ],
-            "end_session": false
-          },
-          "session": {
-            "session_id": "2eac4854-fce721f3-b845abba-20d60",
-            "message_id": 4,
-            "user_id": "AC9WC3DF6FCE052E45A4566A48E6B7193774B84814CE49A922E163B8B29881DC"
-          },
-          "version": "1.0"
-        })";
-        res.body() = data;
-        res.prepare_payload();
-        return res;
-    }
-};
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
 
 //------------------------------------------------------------------------------
+
+// Return a reasonable mime type based on the extension of a file.
+boost::beast::string_view
+mime_type(boost::beast::string_view path)
+{
+	using boost::beast::iequals;
+	auto const ext = [&path]
+	{
+		auto const pos = path.rfind(".");
+		if (pos == boost::beast::string_view::npos)
+			return boost::beast::string_view{};
+		return path.substr(pos);
+	}();
+	if (iequals(ext, ".htm"))  return "text/html";
+	if (iequals(ext, ".html")) return "text/html";
+	if (iequals(ext, ".php"))  return "text/html";
+	if (iequals(ext, ".css"))  return "text/css";
+	if (iequals(ext, ".txt"))  return "text/plain";
+	if (iequals(ext, ".js"))   return "application/javascript";
+	if (iequals(ext, ".json")) return "application/json";
+	if (iequals(ext, ".xml"))  return "application/xml";
+	if (iequals(ext, ".swf"))  return "application/x-shockwave-flash";
+	if (iequals(ext, ".flv"))  return "video/x-flv";
+	if (iequals(ext, ".png"))  return "image/png";
+	if (iequals(ext, ".jpe"))  return "image/jpeg";
+	if (iequals(ext, ".jpeg")) return "image/jpeg";
+	if (iequals(ext, ".jpg"))  return "image/jpeg";
+	if (iequals(ext, ".gif"))  return "image/gif";
+	if (iequals(ext, ".bmp"))  return "image/bmp";
+	if (iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
+	if (iequals(ext, ".tiff")) return "image/tiff";
+	if (iequals(ext, ".tif"))  return "image/tiff";
+	if (iequals(ext, ".svg"))  return "image/svg+xml";
+	if (iequals(ext, ".svgz")) return "image/svg+xml";
+	return "application/text";
+}
+
+// Append an HTTP rel-path to a local filesystem path.
+// The returned path is normalized for the platform.
+std::string
+path_cat(
+	boost::beast::string_view base,
+	boost::beast::string_view path)
+{
+	if (base.empty())
+		return path.to_string();
+	std::string result = base.to_string();
+#if BOOST_MSVC
+	char constexpr path_separator = '\\';
+	if (result.back() == path_separator)
+		result.resize(result.size() - 1);
+	result.append(path.data(), path.size());
+	for (auto& c : result)
+		if (c == '/')
+			c = path_separator;
+#else
+	char constexpr path_separator = '/';
+	if (result.back() == path_separator)
+		result.resize(result.size() - 1);
+	result.append(path.data(), path.size());
+#endif
+	return result;
+}
+
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template<
+	class Body, class Allocator,
+	class Send>
+	void
+	handle_request(
+		boost::beast::string_view doc_root,
+		http::request<Body, http::basic_fields<Allocator>>&& req,
+		Send&& send)
+{
+	/*
+	Alice::Request request(req.body());
+	Alice::Response response;
+	callback(request, response);
+	///....
+
+	res.body() = response.ToString();
+	*/
+
+		http::response<http::string_body> res{ http::status::ok, req.version() };
+		res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+		res.set(http::field::content_type, "text/html"); //appl json
+		res.keep_alive(req.keep_alive());
+		res.body() = "Hello, world!!!";
+		res.prepare_payload();
+		return send(std::move(res));
+}
+
+//------------------------------------------------------------------------------
+
+// Report a failure
+void
+fail(boost::system::error_code ec, char const* what)
+{
+	std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// This is the C++11 equivalent of a generic lambda.
+// The function object is used to send an HTTP message.
+template<class Stream>
+struct send_lambda
+{
+	Stream& stream_;
+	bool& close_;
+	boost::system::error_code& ec_;
+
+	explicit
+		send_lambda(
+			Stream& stream,
+			bool& close,
+			boost::system::error_code& ec)
+		: stream_(stream)
+		, close_(close)
+		, ec_(ec)
+	{
+	}
+
+	template<bool isRequest, class Body, class Fields>
+	void
+		operator()(http::message<isRequest, Body, Fields>&& msg) const
+	{
+		// Determine if we should close the connection after
+		close_ = msg.need_eof();
+
+		// We need the serializer here because the serializer requires
+		// a non-const file_body, and the message oriented version of
+		// http::write only works with const messages.
+		http::serializer<isRequest, Body, Fields> sr{ msg };
+		http::write(stream_, sr, ec_);
+	}
+};
+
+// Handles an HTTP server connection
+void
+do_session(
+	tcp::socket& socket,
+	std::shared_ptr<std::string const> const& doc_root)
+{
+	bool close = false;
+	boost::system::error_code ec;
+
+	// This buffer is required to persist across reads
+	boost::beast::flat_buffer buffer;
+
+	// This lambda is used to send messages
+	send_lambda<tcp::socket> lambda{ socket, close, ec };
+
+	for (;;)
+	{
+		// Read a request
+		http::request<http::string_body> req;
+		http::read(socket, buffer, req, ec);
+		if (ec == http::error::end_of_stream)
+			break;
+		if (ec)
+			return fail(ec, "read");
+
+		// Send the response
+		handle_request(*doc_root, std::move(req), lambda);
+		if (ec)
+			return fail(ec, "write");
+		if (close)
+		{
+			// This means we should close the connection, usually because
+			// the response indicated the "Connection: close" semantic.
+			break;
+		}
+	}
+
+	// Send a TCP shutdown
+	socket.shutdown(tcp::socket::shutdown_send, ec);
+
+	// At this point the connection is closed gracefully
+}
+
 std::string getOSEnv(boost::string_view name, boost::string_view default_value)
 {
     const char* e = std::getenv(name.data());
@@ -183,12 +207,26 @@ int main(int /*argc*/, char** /*argv*/)
         auto const address = boost::asio::ip::make_address("0.0.0.0");
         auto const port_arg = getOSEnv("PORT", "5000");
         auto const port = static_cast<unsigned short>(std::atoi(port_arg.c_str()));
-        auto const doc_root = std::make_shared<std::string>("Hello world");
 
-        HelloWorld server(address, port);
-        server.AddStopSignal(SIGINT);
-        server.AddStopSignal(SIGTERM);
-        server.Start();
+        // The io_context is required for all I/O
+		boost::asio::io_context ioc{ 1 };
+
+		// The acceptor receives incoming connections
+		tcp::acceptor acceptor{ ioc, {address, port} };
+		for (;;)
+		{
+			// This will receive the new connection
+			tcp::socket socket{ ioc };
+
+			// Block until we get a connection
+			acceptor.accept(socket);
+
+			// Launch the session, transferring ownership of the socket
+			std::thread{ std::bind(
+				&do_session,
+				std::move(socket),
+				doc_root) }.detach();
+		}
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
